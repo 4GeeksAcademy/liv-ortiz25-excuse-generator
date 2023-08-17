5c53658ba97ae36186578e7c7e345c9bdc53b8b4
Elizabeth Duarte Ortiz <134437416+liv-ortiz25@users.noreply.github.com>
2023-08-17 02:37:53 +0000

    commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,31 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
 window.onload = function() {
   //write your code here
+  for (let i = 0; 1 < who.length; i++) {
+    const aleatorioWho = Math.floor(Math.random() * who.length);
+  }
+  for (let i = 0; 1 < action.length; i++) {
+    const aleatorioAction = Math.floor(Math.random() * action.length);
+  }
+  for (let i = 0; 1 < what.length; i++) {
+    const aleatorioWhat = Math.floor(Math.random() * what.length);
+  }
+  for (let i = 0; 1 < when.length; i++) {
+    const aleatorioWhen = Math.floor(Math.random() * when.length);
+  }
+
   console.log("Hello Rigo from the console!");
 };
