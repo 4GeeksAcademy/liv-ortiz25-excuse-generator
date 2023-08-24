13e744af02cb05b36654750f3dd5e87d642c0117
Elizabeth Duarte Ortiz <134437416+liv-ortiz25@users.noreply.github.com>
2023-08-24 01:07:53 +0000

    finished

--- src/app.js
+++ src/app.js
@@ -5,28 +5,28 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-let who = ["The dog", "My grandma", "His turtle", "My bird"];
-let action = ["ate", "peed", "crushed", "broke"];
-let what = ["my homework", "the keys", "the car"];
-let when = [
-  "before the class",
-  "right on time",
-  "when I finished",
-  "during my lunch",
-  "while I was praying"
-];
-
 window.onload = function generateExcuse() {
   //write your code here
-  let randomwho = Math.floor(Math.random() * who.length);
-  let randomaction = Math.floor(Math.random() * action.length);
-  let randomwhat = Math.floor(Math.random() * what.length);
-  let randomwhen = Math.floor(Math.random() * when.length);
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
 
-  return `${randomwho} ${randomaction} ${randomwhat} ${randomwhen}.`;
+  let randomwho = who[Math.floor(Math.random() * who.length)];
 
-  console.log("Hello Rigo from the console!");
+  let randomaction = action[Math.floor(Math.random() * action.length)];
+
+  let randomwhat = what[Math.floor(Math.random() * what.length)];
+
+  let randomwhen = when[Math.floor(Math.random() * when.length)];
+
+  document.getElementById(
+    "excuse"
+  ).innerHTML = `${randomwho} ${randomaction} ${randomwhat} ${randomwhen}.`;
 };
-
-const excuseDisplay = (document.getElementById("excuse").innerHTML =
-  randomwho + randomaction + randomwhat + randomwhen);
